f657420220d560265abccfdbcee1971d4cf036ab
dineshpunni <dinesh.punni@hotmail.de>
2017-06-16 22:10:43 +0200

    Created CardMesh Prefabs

---
+++ Assets/Scripts/CardsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsManager : MonoBehaviour {
+
+	public List<Sprite> cardImages;
+	public GameObject cardMesh;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
