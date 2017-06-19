34ccc8398314d1ddc2dd15969ed675020a142163
dineshpunni <dinesh.punni@hotmail.de>
2017-06-19 16:07:22 +0200

    Created Card Prefab ( Frontside and backside)

--- Assets/Scripts/CardsManager.cs
+++ Assets/Scripts/CardsManager.cs
@@ -9,11 +9,21 @@ public class CardsManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+		InitializeCardMeshes();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+
+	void InitializeCardMeshes()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			Instantiate(cardMesh, new Vector3(i * 1.3f, 2, 2), Quaternion.identity);
+		}
 	}
 }
