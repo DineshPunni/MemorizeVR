e936380cf24014bb4fc426b00fcebce6540999d0
dineshpunni <dinesh.punni@hotmail.de>
2017-06-30 20:56:16 +0200

    Added CubeController with teleportCube script

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	public float x;
+	public float y;
+	public float z;
+
+
+	public void TeleportCube()
+	{
+		float newX = Random.Range(-x, x);
+		float newY = Random.Range(0, y);
+		float newZ = Random.Range(-z, z);
+
+		transform.position = new Vector3(newX, newY, newZ);
+	}
+}
