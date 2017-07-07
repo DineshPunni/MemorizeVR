572d7a288933a1f1113138ba91f621c3ce22a4cf
dineshpunni <dinesh.punni@hotmail.de>
2017-07-07 20:46:44 +0200

    GameOver mechanic finally handled (with actions)

---
+++ Assets/BallControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+	void Start () {
+
+		Invoke("SelfDestruct", 3);
+	}
+	
+	public void SelfDestruct()
+	{
+		Destroy(gameObject);
+	}
+	
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,8 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	
+
+	public GameObject GameOverPrefab;
+	GameObject enemy;
+
+
+	void OnEnable()
+	{
+		EnemyControl.OnGameOver += GameOver;
+	}
+
+	void GameOver()
+	{
+		enemy.SetActive(false);
+		SpawnGameOverCanvas();
+	}
+
+	void OnDisable()
+	{
+		EnemyControl.OnGameOver -= GameOver;
+	}
+
+	void Start()
+	{
+		enemy = FindObjectOfType<EnemyControl>().gameObject;
+	}
+
+
+
+	public void Restart()
+	{
+		GameOverPrefab.SetActive(false);
+		//Instantiate(enemy, new Vector3(0, 0, 5), Quaternion.identity);
+		enemy.SetActive(true);
+		enemy.GetComponent<EnemyControl>().TeleportEnemy();
+	}
+
+
+	void SpawnGameOverCanvas()
+	{
+		//GameObject tmp = Instantiate(GameOverPrefab, transform.position, Quaternion.identity);
+		GameOverPrefab.SetActive(true);
+		GameOverPrefab.transform.LookAt(Camera.main.transform);
+		GameOverPrefab.transform.Rotate(new Vector3(0, 180, 0));
+		GameOverPrefab.transform.Translate(Vector3.forward * 5);
+	}
 }
